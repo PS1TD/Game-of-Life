16935e5178d5a39deeb28c6bfe7b6a8f5ce3bda8
Alexander Vorobyev <vent1narc@gmail.com>
2021-06-16 17:53:55 +0300

    ♾ Rows and Columns

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Dashboard() {
 	return (
-		<div className="fixed bottom-0 w-full">
+		<div className="fixed bottom-0 w-full pointer-events-none">
 			<div className="flex justify-around mb-16">
 				<button className="bg-green-400 p-6 rounded-full">START/STOP</button>
 				<button className="bg-red-400 p-6 rounded-full">CLEAR</button>
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useRef, useState } from "react"
 import Cell from "./Cell"
 
 export default function Grid() {
-	const [rows, setRows] = useState(30)
-	const [columns, setColumns] = useState(50)
+	const [rows, setRows] = useState(1)
+	const [columns, setColumns] = useState(2)
 
 	const [grid, setGrid] = useState<Array<Array<boolean>>>(Array.from(Array(rows), () => Array(columns).fill(false)))
 
@@ -13,7 +13,7 @@ export default function Grid() {
 		var options = {
 			root: null,
 			rootMargin: "20px",
-			threshold: 1.0,
+			threshold: 0,
 		}
 		const rowObserver = new IntersectionObserver(handleRowObserver, options)
 		if (rowLoader.current) {
@@ -29,30 +29,64 @@ export default function Grid() {
 
 	const handleRowObserver = (entities: any) => {
 		const target = entities[0]
+		console.log(target)
 		if (target.isIntersecting) {
-			console.log("HIT")
-			setRows((rows) => rows + 1)
+			console.log("ROWS HIT")
+			setRows((rows) => rows + 10)
+		}
+	}
+
+	const columnLoader = useRef(null)
+
+	useEffect(() => {
+		var options = {
+			root: null,
+			rootMargin: "20px",
+			threshold: 0,
+		}
+		const columnObserver = new IntersectionObserver(handleColumnObserver, options)
+		if (columnLoader.current) {
+			columnObserver.observe(columnLoader.current!)
+		}
+	}, [])
+
+	useEffect(() => {
+		console.log("ADD MORE COLUMNS")
+		const newGrid = Array.from(Array(rows), () => Array(columns).fill(false))
+		setGrid(newGrid)
+	}, [columns])
+
+	const handleColumnObserver = (entities: any) => {
+		const target = entities[0]
+		if (target.isIntersecting) {
+			console.log("COLUMNS HIT")
+			setColumns((columns) => columns + 10)
 		}
 	}
 
 	return (
 		<>
-			<div>
-				{grid.map((row, r) => {
-					{
-						return (
-							<div className="block whitespace-nowrap">
-								{row.map((cell, c) => {
-									{
-										return <Cell key={r + "-" + c} row={r} column={c} />
-									}
-								})}
-							</div>
-						)
-					}
-				})}
+			<div className="flex flex-col" style={{ width: `${columns * 100}px` }}>
+				<div className="flex">
+					<div>
+						{grid.map((row, r) => {
+							{
+								return (
+									<div className="flex">
+										{row.map((cell, c) => {
+											{
+												return <Cell key={r + "-" + c} row={r} column={c} />
+											}
+										})}
+									</div>
+								)
+							}
+						})}
+					</div>
+					<div ref={columnLoader}></div>
+				</div>
+				<div className="flex" ref={rowLoader}></div>
 			</div>
-			<div ref={rowLoader}></div>
 		</>
 	)
 }
