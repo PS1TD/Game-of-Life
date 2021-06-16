eaaf73b6358132be2aef5ef17e0bdacdb6892076
Alexander Vorobyev <vent1narc@gmail.com>
2021-06-17 01:28:41 +0300

    🏬 Persist Store or Reload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,17 @@ import React from "react"
 import { Provider } from "react-redux"
 import Dashboard from "../components/Dashboard"
 import Grid from "../components/Grid"
-import { store } from "../redux/store"
+import { store, persistor } from "../redux/store"
+
+import { PersistGate } from "redux-persist/integration/react"
 
 export default function index() {
 	return (
 		<Provider store={store}>
-			<Grid />
-			<Dashboard />
+			<PersistGate loading={null} persistor={persistor}>
+				<Grid />
+				<Dashboard />
+			</PersistGate>
 		</Provider>
 	)
 }
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -2,4 +2,15 @@ import { composeWithDevTools } from "redux-devtools-extension"
 import { createStore } from "redux"
 import { rootReducer } from "../reducers"
 
-export const store = createStore(rootReducer, composeWithDevTools())
+import storage from "redux-persist/lib/storage"
+import { persistReducer, persistStore } from "redux-persist"
+
+const persistConfig = {
+	key: "root",
+	storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer, composeWithDevTools())
+export const persistor = persistStore(store)
